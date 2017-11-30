21c6baf47513c7f8dced4743ca20fddb1f9c1f20
Xuefei Cao <xuefei_cao@brown.edu>
2017-11-29 19:47:20 -0500

    scale, shifted, toy example

--- utility/data_generate_toy_example.py
+++ utility/data_generate_toy_example.py
@@ -8,39 +8,30 @@ from scipy.integrate import simps
 import matplotlib.pyplot as plt
 import scipy.sparse as sp
 #this is specific for stop and go fMRI data
-def CDN_generate(seed, n_area=6, SNR=0, A_u=True, B_u=True, C_u=True, D_u=True, which_data=-1, scale=1, shifted=1):
+def Toy_generate(seed, n_area=1, SNR=0, which_data=1, scale = 1, shifted = 1):
 
     """ define a function to generate data from CDN model, please download precomp_0.Rdata 
     para: seed for random number
           n_area: dimension of the system
-          SNR: noise added to the system ,i.e. SNR
-          A_u: boolean variable for whether you want to update A
-          B_u: ...
-          C_u: ...
-          D_u: ... 
+          SNR: noise added to the system ,i.e. SNR 
           which_data: 1 (negative definite matrix), 2 (total random with negative eigenvalues), 3 (antisymmetric), -1 (default, random select one scenario)
-          scale: the scale of A, B, C, D, default=1 i.e original scale
-          shifted: time step shift
-    return: fmri data(dimension: n_area * length), fmri_shifted(dimension: n_area * length)-padding 0, stimuli(dimension: J * length)), A, B, C, D
+          scale: scale of A, D
+          shifted : 1, shifted time step 
+          Note: for this toy example, which data must be 1 !!!
+    return: fmri data(dimension: n_area * length), fmri_shifted(dimension: n_area * length)-padding 0,  A, D
     length means the length of time series
     """
-    #load precomputed data
+    #
     robjects.r.load('precomp_0.rdata')
-    t_1 = np.array(robjects.r['x'][20])
-    l_t_1 = t_1.shape[0]
     hrf = np.array(robjects.r['x'][21])
-    t = np.array(robjects.r['x'][23])
-    Q4_1 = np.array(robjects.r['x'][27])
-    t_all = np.array(robjects.r['x'][28])
-    t_U_1 = np.array(robjects.r['x'][29])
-    t_i_all = t_all
-    t_i = t
-    l_t_all = t_i_all.shape[0]
-    row_n = int(np.array(robjects.r['x'][30])[0])
-    l_t_0 = row_n 
-    J = int(np.array(robjects.r['x'][31])[0])
-    dt = np.array(robjects.r['x'][34])[0]
-    fold = np.array(robjects.r['x'][35])[0]
+    t_1 = np.array(robjects.r['x'][20])
+    fold = 0.25
+    dt = 0.01
+    row_n = 201
+    l_t_all = (row_n - 1) * int(1 / fold) + 1
+    l_t_0 = row_n
+    l_t_1 = hrf.shape[0]
+
 
     #generate random parameters
     np.random.seed(seed)
@@ -85,47 +76,22 @@ def CDN_generate(seed, n_area=6, SNR=0, A_u=True, B_u=True, C_u=True, D_u=True, 
         return tmp
 
     A = np.zeros((n_area,n_area))
-    B = np.zeros((n_area,n_area,J))
-    C = np.zeros((n_area,J))
     D = np.zeros((n_area,1))
 
 
-    if A_u:
-        A = para_gen(n_area)
-    if B_u:
-        for j in range(J):
-            B[:,:,j] = para_gen(n_area)
-    if C_u:
-        C = np.random.uniform(-1, 1, size=(n_area,J)) * scale
-    if D_u:
-        D = np.random.uniform(-50, 50, size=(n_area,1)) * scale
+    A = para_gen(n_area)
+    D = np.random.uniform(-50, 50, size=(n_area,1)) * scale
 
-    def ODE_solve(A, B, C, D):
+    def ODE_solve(A, D):
         h = fold * dt
         x_l = np.zeros((n_area,l_t_all))
         x_l[:,0] = D.reshape((-1))
         for i in range(1,l_t_all):
-            tmp = 0
-            for j in range(J):
-                tmp = tmp + Q4_1[j,i-1] * np.dot(B[:,:,j], x_l[:,i-1])
-            k1 = np.dot(A, x_l[:,i-1]) + tmp + np.dot(C, Q4_1[:,i-1])
-            tmp = 0
-            for j in range(J):
-                tmp = tmp + t_U_1[j,i-1] * np.dot(B[:,:,j], (x_l[:,i-1] + h/2 * k1))
-            k2 = np.dot(A, (x_l[:,i-1] + h/2 * k1)) + tmp + np.dot(C, t_U_1[:,i-1])
-            tmp=0
-            for j in range(J):
-                tmp = tmp + t_U_1[j,i-1] * np.dot(B[:,:,j], (x_l[:,i-1] + h/2 * k2))
-            k3 = np.dot(A, (x_l[:,i-1] + h/2 * k2)) + tmp + np.dot(C, t_U_1[:,i-1])
-            tmp = 0
-            for j in range(J):
-                tmp = tmp + Q4_1[j,i] * np.dot(B[:,:,j], (x_l[:,i-1] + h * k3))
-            k4 = np.dot(A, (x_l[:,i-1] + h * k3)) + tmp + np.dot(C, Q4_1[:,i])
-            x_l[:,i] = x_l[:,i-1] + 1.0 * h / 6 * (k1 + 2 * k2+2 * k3 + k4)
+            x_l[:,i] = x_l[:,i-1] + h * np.dot(A, x_l[:,i-1]) 
         return x_l
 
-    def fmri_data(A, B, C, D, SNR):
-        x_l = ODE_solve(A, B, C, D)
+    def fmri_data(A, D, SNR):
+        x_l = ODE_solve(A, D)
         z = np.zeros((n_area,l_t_0))
         for j in range(l_t_0):
             tmp = np.zeros((n_area, l_t_1))
@@ -141,9 +107,10 @@ def CDN_generate(seed, n_area=6, SNR=0, A_u=True, B_u=True, C_u=True, D_u=True, 
         z = z + np.random.normal(0, sd * SNR, z.shape)
         return z
 
-    y = fmri_data(A, B, C, D, SNR)
+    y = fmri_data(A, D, SNR)
     shifted_y = np.zeros(y.shape)
     shifted_y[:,0:(row_n-shifted)] = y[:,shifted::]
 
-    return y, shifted_y, Q4_1[:,::int(1 / fold)], A, B, C, D 
+
+    return y, shifted_y, A, D 
 
